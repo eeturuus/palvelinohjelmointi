cbaad3d90aa73d2c27f55dce26c397847078a309
eeturuus <bgm050@myy.haaga-helia.fi>
2021-03-11 13:16:42 +0200

    First commit

---
+++ src/main/java/fi/haagahelia/course/domain/Book.java
@@ -0,0 +1,101 @@
+package fi.haagahelia.course.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String author;
+	private String title;
+    private int year;
+    private String isbn;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryid")
+    private Category category;
+    
+    public Book() {}
+    
+    
+    public Book(String author, String title, int year, String isbn, double price, Category category) {
+		super();
+		this.author = author;
+		this.title = title;
+		this.year = year;
+		this.isbn = isbn;
+		this.category = category;
+	}
+
+    public Long getId() {
+    	return id;
+    }
+    public void setId(Long id) {
+    	this.id = id;
+    }
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	
+    @Override
+	public String toString() {
+		return "Book [id=" + id + ", author=" + author + ", title=" + title + ", year=" + year + ", isbn=" + isbn
+				+ ", price=" + price + "]";
+	}
+
+
+
+	
+}
---
+++ src/main/java/fi/haagahelia/course/web/BookController.java
@@ -0,0 +1,73 @@
+package fi.haagahelia.course.web;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fi.haagahelia.course.domain.Book;
+import fi.haagahelia.course.domain.BookRepository;
+import fi.haagahelia.course.domain.CategoryRepository;
+
+@Controller
+public class BookController {
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CategoryRepository crepository;
+	
+    @RequestMapping(value= {"/booklist"})
+    public String bookList(Model model) {
+    	System.out.println("OLLAAN BOOKLIST ENDPOINTISSA");
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
+    }
+  
+    // RESTful service to get all books
+    @RequestMapping(value="/books", method = RequestMethod.GET)
+    public @ResponseBody List<Book> bookListRest() {
+    	return (List<Book>) repository.findAll();
+    }
+    
+    // RESTful service to get book by id
+    @RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+		return repository.findById(bookId);
+	}
+    
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+    	model.addAttribute("categories", crepository.findAll());
+        return "addbook";
+    }     
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
+
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist"; //redirect:booklist
+    }
+    
+    @RequestMapping(value="/modify/{id}", method=RequestMethod.GET)
+	public String modifyBook(@PathVariable("id") Long bookId, Model model) {
+		Optional<Book> book = repository.findById(bookId);
+		model.addAttribute("book", book);
+		model.addAttribute("categories", crepository.findAll());
+		System.out.println("OLLAAN MODIFYBOOK METODISSA");
+		System.out.println(book);
+    	return "modifybook";
+	}
+}
