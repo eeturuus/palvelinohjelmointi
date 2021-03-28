3f941fa81f728b8aeb1d284b4ac84da82415ac27
eeturuus <bgm050@myy.haaga-helia.fi>
2021-03-28 13:24:52 +0300

    pom.xml, application.properties ja muuta tuli säädettyä

--- src/test/java/fi/haagahelia/course/BookListApplicationTests.java
+++ src/test/java/fi/haagahelia/course/BookListApplicationTests.java
@@ -2,14 +2,16 @@ package fi.haagahelia.course;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 
 import fi.haagahelia.course.domain.Book;
 import fi.haagahelia.course.domain.Category;
@@ -17,6 +19,7 @@ import fi.haagahelia.course.web.BookController;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
+@SpringBootTest
 public class BookListApplicationTests {
 
 	@Autowired
@@ -24,6 +27,7 @@ public class BookListApplicationTests {
 	
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
